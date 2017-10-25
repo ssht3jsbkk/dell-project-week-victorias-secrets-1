b8cd1d82da90981d2a9fb1c8ea5a8d97d6e1ef6d
victoriabate <victoriabate99@gmail.com>
2017-10-25 11:28:38 +0100

    committing changes to server

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post('/Company', (req, res) => {
   })
 })
 app.post('/Notes', (req, res) => {
-  const note = new Note(req.body);
+  const note = new Notes(req.body);
   note.save(req.body, (err, result) => {
     if (err) return console.log(err)
    console.log('saved to database')
